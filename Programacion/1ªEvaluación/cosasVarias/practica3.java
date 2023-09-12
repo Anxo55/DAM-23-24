2a3d6783535351468a7175a6fccc09e9aa8e7c62
Anxo Campos <anxo.campos260204@gmail.com>
2023-09-12 17:50:46 +0200

    all

--- Programacion/1ªEvaluación/cosasVarias/practica3.java
+++ Programacion/1ªEvaluación/cosasVarias/practica3.java
@@ -1,57 +1,82 @@
 package cosasVarias;
 
+import java.util.Scanner;
+
 // Clase donde vamos a introducir varios metodos, en el cual escogeremos el que vamos a usar.
 // Utilizaremos un metodo switch donde incluiremos los metodos de suma, resta, multiplicacion y division.
 
 public class practica3 {
-    
+
     public static void main(String[] args) {
-        double num1 = 10.0;
-        double num2 = 5.0;
-        char operacion = '+'; // Cambia este carácter para seleccionar la operación: +, -, *
 
-        double resultado = calcular(num1, num2, operacion);
-        System.out.println("Resultado: " + resultado);
-    }
+        Scanner sc = new Scanner(System.in);
 
-    public static double calcular(double num1, double num2, char operacion) {
-        double resultado = 0.0;
+        System.out.println("###############");
+        System.out.println("##CALCULADORA##");
+        System.out.println("###############");
+
+        System.out.println("");
+
+        System.out.println("Ingresa el primer numero: ");
+        int numero1=sc.nextInt();
+
+        System.out.println("Ingresa el segundo numero: ");
+        int numero2=sc.nextInt();
+        
+        System.out.println("(1:suma)");
+        System.out.println("");
+        System.out.println("(2:resta)");
+        System.out.println("");
+        System.out.println("(3:multiplicacion)");
+        System.out.println("");
+        System.out.println("(4:division)");
+        int operacion = sc.nextInt();
 
         switch (operacion) {
-            case '+':
-                resultado = sumar(num1, num2);
-                break;
-            case '-':
-                resultado = restar(num1, num2);
-                break;
-            case '*':
-                resultado = multiplicar(num1, num2);
-                break;
-                case'/':
-                resultado = dividir(num1, num2);
-                break;
+            case 1:
+            int suma=sumar(numero1,numero2);
+            System.out.println("El resultado de la suma es: "+suma);
+            break;
+
+            case 2:
+            int resta=restar(numero1,numero2);
+            System.out.println("El resultado de la resta es: "+resta);
+            break;
+
+            case 3:
+            int multiplicacion=multiplicar(numero1,numero2);
+            System.out.println("El resultado de la multiplicacion es: "+multiplicacion);
+            break;
+
+            case 4:
+            int division=dividir(numero1, numero2);
+            System.out.println("El resultado de la division es>: "+division);
+            break;        
+
             default:
-                System.out.println("Operación no válida");
+            System.out.println("Opcion no valida....");
+                break;
         }
+        
 
-        return resultado;
+    }
+    public static int sumar(int a, int b) {
+        return a+b;
     }
 
-    public static double sumar(double num1, double num2) {
-        return num1 + num2;
+    public static int restar(int a, int b) {
+        return a-b;
     }
 
-    public static double restar(double num1, double num2) {
-        return num1 - num2;
+    public static int multiplicar(int a, int b) {
+        return a*b;
     }
 
-    public static double multiplicar(double num1, double num2) {
-        return num1 * num2;
+    public static int dividir(int a, int b) {
+        return a/b;
     }
-
-    public static double dividir(double num1, double num2) {
-        return num1 / num2; 
-    }
+    
+    
 }
     
 
