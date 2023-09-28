25d555284e1b81edb9aea5133693553891e1db86
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2023-09-28 12:43:24 +0200

    all

---
+++ entornos/1ªEvaluacion/practica/dividir.java
@@ -0,0 +1,27 @@
+package practica;
+
+import java.util.Scanner;
+
+public class dividir {
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int num1, num2;
+
+       System.out.println("Introduce el dividendo");
+       num1 = sc.nextInt();
+
+       do {
+           System.out.println("Introduce el divisor");
+           num2 = sc.nextInt();
+           if (num2 == 0)
+               System.out.println("No se puede dividir entre 0, inténtalo de nuevo");
+
+       } while (num2 == 0);
+
+       System.out.println("El cociente de la división es " + num1 / num2);
+       System.out.println("El resto de la división es " + num1 % num2);
+       
+   
+       
+   }
+}
---
+++ entornos/1ªEvaluacion/practica/division.java
@@ -0,0 +1,34 @@
+package practica;
+
+import java.util.Scanner;
+
+public class division {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        double division, resto, cociente;
+
+        System.out.println("Dame el dividendo: ");
+        double dividendo = sc.nextDouble();
+
+        System.out.println("Dame el divisor: ");
+        double divisor = sc.nextDouble();
+
+        cociente= dividendo/divisor;
+        resto = dividendo % divisor;
+
+        System.out.println("#######################");
+        System.out.println("##### RESULTADO #######");
+        System.out.println("#######################");
+        System.out.println("##El cociente es: "+cociente+"##");
+        System.out.println("#######################");
+        System.out.println("###El resto es: "+resto+"####");
+        System.out.println("#######################");
+
+
+
+    }
+    
+}
