3f0011d97bfa2775aabcd60282a5418914d7f4b5
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2023-10-16 22:40:43 +0200

    practicaProgramacion

--- repasoGeneralProgramacion/ChatGPT/POO/Libro.java
+++
@@ -1,9 +0,0 @@
-package ChatGPT.POO;
-
-//  Crea una clase Libro con atributos como título, autor y año
-//   de publicación. Implementa métodos para obtener y establecer 
-//   esta información.
-
-public class Libro {
-    
-}
--- repasoGeneralProgramacion/ChatGPT/POO/Vehiculo.java
+++
@@ -1,9 +0,0 @@
-package ChatGPT.POO;
-
-// Diseña una clase Vehículo que tenga atributos como marca, 
-// modelo y año. Crea métodos para acelerar y frenar el vehículo, 
-// y para mostrar su información.
-
-public class Vehiculo {
-    
-}
