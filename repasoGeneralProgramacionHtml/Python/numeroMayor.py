f88c7f4e593c27512b2150a84aac3af699696fea
Anxo Campos <anxo.campos260204@gmail.com>
2023-10-21 17:56:53 +0200

    all

---
+++ repasoGeneralProgramacionHtml/Python/numeroMayor.py
@@ -0,0 +1,10 @@
+print("Quiero crear una clase donde pasando dos numeros me diga cual de los dos es mayor")
+
+numero1 = int(input("Dame el primer entero: "))
+
+numero2 = int(input("Dame el segundo entero: "))
+
+if numero1 > numero2 : 
+    print("El numero "+str(numero1)+" es mayor que el numero "+str(numero2))
+else :
+    print("El numero "+str(numero2)+ " es mayor que el numero "+str(numero1))
---
+++ repasoGeneralProgramacionHtml/Python/sumaNumeros.py
@@ -0,0 +1,10 @@
+print("Quiero la suma de dos numeros enteros")
+
+numero1 = int(input("Dame el numero 1: "))
+
+numero2 = int(input("Dame el numero 2: "))
+
+suma = numero1 + numero2
+
+# Usando str() para convertir suma a cadena de texto:
+print("El resultado de la suma es "+str(suma))
