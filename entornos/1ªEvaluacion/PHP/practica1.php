5e7af719aa52636742ece3bb5419e97c96604067
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2023-10-24 12:36:22 +0200

    PHP

---
+++ entornos/1ªEvaluacion/PHP/practica1.php
@@ -0,0 +1,18 @@
+<?php
+
+$numero1 = 10;
+$numero2 = 20;
+
+$suma = $numero1 + $numero2;
+echo "La suma de ".$numero1. " y " .$numero2. " = ".$suma;
+echo "</br>";
+echo "Calculamos cual de los dos numeros es mayor";
+echo "</br>";
+
+if ($numero1 > $numero2) {
+    echo "El numero " .$numero1. " es mayor que " .$numero2;
+}else if($numero1 < $numero2 ) {
+    echo "El numero ".$numero2. " es mayor que ".$numero1;
+}
+
+?>
