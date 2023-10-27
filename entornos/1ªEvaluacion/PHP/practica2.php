b7c6dfe1a9c877da163555f89b0275c6bd37cb96
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2023-10-27 10:54:27 +0200

    PHP

---
+++ entornos/1ªEvaluacion/PHP/practica2.php
@@ -0,0 +1,16 @@
+<?php
+
+// $numero1 =readline("Ingresa el primer numero: ")
+// $numero2 = readline("Ingresa el segundo numero: ")
+
+$numero1 = 2;
+$numero2 = 2;
+
+if ($numero1 + $numero2 == 4) {
+    print("Si");
+}else{
+    print("No");
+}
+
+
+?>
