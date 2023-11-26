f4557c68c976a73558cb659759f6f0c074ed2199
Anxo Campos <anxo.campos260204@gmail.com>
2023-11-26 15:56:53 +0100

    pruebaBinario

---
+++ Programacion/1ªEvaluación/pruebaRepaso/Ejercicio4B.java
@@ -0,0 +1,37 @@
+package pruebaRepaso;
+
+import java.util.Scanner;
+
+//Pasar un numero de sistema decimal a Binario  mediante el siguiente metodo:
+//dividiendo sucesivamente entre dos y quedandose con el cociente, hasta la ultima division
+//donde el resto ya solo sera cero o uno. El numero convertido se construye desde ese ultimo
+//resto anhadiendo despues de los cocientes que hemos ido obteniendo.
+
+public class Ejercicio4B {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero;
+
+        System.out.println("Dame el valor del numero entero: ");
+        numero = sc.nextInt();
+
+       System.out.println("El numero decimal " + numero + " en binario es: " + decimalToBinario(numero));
+    }
+
+    public static String decimalToBinario(int numero) {
+        
+        if (numero == 0) {
+            return "0";
+        }
+
+        String binario = "";
+        while (numero > 0) {
+            int residuo = numero % 2;
+            binario = residuo + binario;
+            numero /= 2;
+        }
+
+        return binario;
+    }
+}
