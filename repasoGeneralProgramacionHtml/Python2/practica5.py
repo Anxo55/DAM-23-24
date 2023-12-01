7d748b42693786a0a7095b7da9103c526bad239b
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2023-12-01 18:28:31 +0100

    cosasVarias

---
+++ repasoGeneralProgramacionHtml/Python2/practica5.py
@@ -0,0 +1,11 @@
+# varias condiciones
+print("Practicas de condiciones")
+
+numero = int(input("Dime numero: "))
+
+if numero > 0:
+    print("El numero "+str(numero)+" es positivo")
+elif numero ==0:
+    print("El numero es 0")
+else:
+    print("El numero "+str(numero)+" es negativo")
