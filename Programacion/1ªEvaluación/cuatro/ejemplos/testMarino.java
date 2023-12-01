7fba5a80043d1df056a9246011e6d41888a03c6d
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2023-12-01 17:47:33 +0100

    practicaHerencias

---
+++ Programacion/1ªEvaluación/cuatro/ejemplos/testMarino.java
@@ -0,0 +1,17 @@
+package cuatro.ejemplos;
+
+public class testMarino {
+
+    public static void main(String[] args) {
+
+        // crear un pez
+
+        // crear un espada
+
+        // crear un martillo
+
+        // Probar los metodos nada y soy comestible
+        
+    }
+    
+}
