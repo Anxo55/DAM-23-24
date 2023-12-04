a5e6f664fd97f96e75027e87ff61bb1475fa2666
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2023-12-04 20:38:51 +0100

    repasoGeneral

--- Programacion/1ªEvaluación/cuatro/propuestos/Propuesto1.java
+++ Programacion/1ªEvaluación/cuatro/propuestos/Propuesto1.java
@@ -13,36 +13,37 @@ import java.util.Scanner;
 
 public class Propuesto1 {
 
-    public static void ver(int arra[][]) {
-
+   public static void ver(int arra[][]) {
         int filas = arra.length;
-        // int arr[][] = arra[][];
-
-        for(int i=0; i<filas; i++) {
-            for(int j=0; j<filas; j++) {
-                System.out.print(arra[i][j]+ " ");
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < filas; j++) {
+                System.out.print(arra[i][j] + " ");
             }
             System.out.println();
         }
-        
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int filas;
-        
+
         System.out.println("Dime las filas: ");
         filas = sc.nextInt();
 
-        int arr[][] = new int[filas][filas]; 
+        int arr[][] = new int[filas][filas];
 
-        ver(arr);
-
-        /*for(int k=0; k<filas; k++) {
-            System.out.println(Arrays.toString(arr[k]));
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || i == j) {
+                    arr[i][j] = 1;
+                } else {
+                    arr[i][j] = arr[i - 1][j] + arr[i - 1][j - 1];
+                }
+            }
         }
-        */
-        
+
+        // Mostrar la matriz
+        System.out.println("Matriz generada:");
+        ver(arr);
     }
-    
 }
