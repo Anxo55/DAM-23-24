392d2b6097a37c5b09fbee8f38fa98eb14d6f1a7
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2024-01-09 11:40:57 +0100

    teoriaSQL

--- Bases de datos/2ªEvaluacion/MySql/Practica/LenguajeSQL/Practica2.sql
+++ Bases de datos/2ªEvaluacion/MySql/Practica/LenguajeSQL/Practica2.sql
@@ -9,15 +9,17 @@ IdAutor INT,
 IdTema INT,
 AnhoPublicacion SMALLINT,
 DiasMaxPrestamo INT);
-
+DROP TABLE ejemplares;
 CREATE TABLE ejemplares(
 IdLibro INT,
-IdEjemplar INT PRIMARY KEY,
+IdEjemplar INT,
 Ubicacion CHAR(15),
 Edicion CHAR(15),
 Precio DOUBLE,
 Disponible BOOL,
-CONSTRAINT PRIMARY KEY (IdLibro) REFERENCES libros);
+PRIMARY KEY (IdLibro,IdEjemplar),
+FOREIGN KEY(IdLibro) REFERENCES libros(IdLibro),INDEX (IdLibro),INDEX(IdEjemplar))
+;
 
 CREATE TABLE socios(
 IdSocio INT PRIMARY KEY,
@@ -27,6 +29,7 @@ Direccion VARCHAR(50),
 Telefono CHAR(9),
 Sexo ENUM('Hombre','Mujer'));
 
+DROP TABLE prestamos;
 CREATE TABLE Prestamos(
 IdPrestamo INT PRIMARY KEY,
 IdLibro INT,
@@ -34,7 +37,9 @@ IdEjemplar INT,
 IdSocio INT,
 FechaPrestamo DATE,
 FechaDevolucion DATE,
-Devuelto BOOLEAN);
+ CONSTRAINT fk1 FOREIGN KEY (IdLibro) REFERENCES ejemplares(IdLibro),
+ CONSTRAINT fk2  FOREIGN KEY (IdEjemplar) REFERENCES ejemplares (IdEjemplar),
+ FOREIGN KEY (idSocio) REFERENCES socios(IdSocio));
 
 
 
