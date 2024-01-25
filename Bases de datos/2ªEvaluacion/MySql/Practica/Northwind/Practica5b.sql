6f8ff1e2eb079bbb49fd73a70e7052528e5846be
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2024-01-25 10:10:58 +0100

    practicaNorthwind

--- Bases de datos/2ªEvaluacion/MySql/Practica/Northwind/Practica5b.sql
+++ Bases de datos/2ªEvaluacion/MySql/Practica/Northwind/Practica5b.sql
@@ -29,60 +29,114 @@ FROM Employees;
 #empleados, éste deberá tener los siguientes datos:
 #a. La primera letra del nombre, seguida del apellido.
 #b. El correo deberá presentarse en minúscula.
-SELECT  CONCAT(LEFT(FirstName, 1), ' ', LastName) AS nombre_completo
+SELECT  CONCAT(LEFT(FirstName, 1),LastName, "@gmail.com") AS nombre_completo
 FROM Employees;
 
 #Ejercicio 207
 #Realiza una consulta que permita presentar el nombre y apellidos de los empleados que tengan más
 #de 40 años.
+SELECT FirstName, LastName
+FROM Employees
+WHERE DATEDIFF(CURDATE(), Birthdate) / 365 > 40;
 
 #Ejercicio 208
 #Realiza una consulta que permita presentar el número de fax que tiene cada país.
+SELECT Country, COUNT(Fax) AS Numero_de_Fax
+FROM Customers
+WHERE Fax IS NOT NULL AND Fax <> ''
+GROUP BY Country;
+
 #Ejercicio 209
 #Realiza una consulta que permita presentar el máximo, mínimo y el promedio de años que llevan en
 #la empresa los empleados.
+SELECT 
+    MAX(YEAR(NOW()) - YEAR(HireDate)) AS Max_Anios_En_Empresa,
+    MIN(YEAR(NOW()) - YEAR(HireDate)) AS Min_Anios_En_Empresa,
+    AVG(YEAR(NOW()) - YEAR(HireDate)) AS Promedio_Anios_En_Empresa
+FROM Employees;
 
 #Ejercicio 210
 #Realiza una consulta que permita presentar un listado de países que no tienen faxes con su
 #respectiva cantidad de clientes.
+
+SELECT Country, COUNT(CustomerID) AS Cantidad_de_Clientes
+FROM Customers
+WHERE Fax IS NULL OR Fax = ''
+GROUP BY Country;
+
 #Ejercicio 210 bis
 #Realiza una consulta que permita presentar un listado de países que no tienen faxes con su
 #respectiva cantidad de clientes.
 
 #Ejercicio 211
 #Realiza una consulta que permita presentar el nombre del cliente, el fax y el país al que pertenece
+SELECT ContactName, Fax, Country
+FROM Customers;
 
 #Ejercicio 212
 #Realiza una consulta que muestre la mayor edad, la menor edad y la edad promedio de los
 #empleados. Recuerde que para eso tiene que buscar la diferencia entre la fecha actual y la fecha de
 #nacimiento.
+SELECT 
+    MAX(YEAR(NOW()) - YEAR(BirthDate)) AS Mayor_Edad,
+    MIN(YEAR(NOW()) - YEAR(BirthDate)) AS Menor_Edad,
+    AVG(YEAR(NOW()) - YEAR(BirthDate)) AS Edad_Promedio
+FROM Employees;
+
 #Ejercicio 213
 #Realiza una consulta que muestre la cantidad de facturas que se han registrado por cada año (order
 #date).
+SELECT YEAR(OrderDate) AS Anio, COUNT(OrderID) AS Cantidad_de_Facturas
+FROM Orders
+GROUP BY YEAR(OrderDate)
+ORDER BY Anio;
 
 #Ejercicio 214
 #Realiza una consulta que muestre la cantidad de facturas que se han registrado por cada año, mes y
 #empleado.
+SELECT 
+    YEAR(fecha) AS Anio,
+    MONTH(fecha) AS Mes,
+    empleado AS Empleado,
+    COUNT(*) AS CantidadFacturas
+FROM  Orders
+GROUP BY YEAR(fecha), MONTH(fecha), empleado
+ORDER BY Anio, Mes, Empleado;
 
 #Ejercicio 215
 #Realiza una consulta que me permita mostrar el número de la factura y el valor por concepto de
 #envío.
+SELECT ShipVia, Freight
+FROM Orders;
 
 #Ejercicio 216
 #Realiza una consulta que me permita mostrar el número de la factura y el valor por concepto de
 #envío pero de aquellos que superen a los 10.
+SELECT ShipVIa, Freight
+FROM Orders
+WHERE Freight > 10;
 
 #Ejercicio 217
 #Realiza una consulta que muestre el código de la categoría, el total de productos que tiene la
 #categoría únicamente para las categorías que tengan más de 10 productos. Para esto utilice la
 #cláusula having.
+SELECT CategoryID, COUNT(*) AS TotalProductos
+FROM  products
+GROUP BY CategoryID
+HAVING COUNT(*) > 10;
 
 #Ejercicio 218
 #Realizar una consulta que permita presentar el nombre del producto y con su respetiva categoría.
+SELECT ProductName, CategoryID
+FROM Products;
 
 #Ejercicio 219
 #Realizar una búsqueda de los nombres de compañías con las cuales se ha tenido ventas, ordenar por
 #el código del vendedor.
+SELECT CompanyName, SupplierID
+FROM Suppliers
+ORDER BY SupplierID;
+
 
 #Ejercicio 220
 #Realiza una consulta que muestre el listado de las facturas de cada cliente. El listado debe tener las
@@ -92,3 +146,13 @@ FROM Employees;
 #Ejercicio 221
 #Búsqueda de los nombres de compañías con las cuales se ha tenido ventas, ordenar por el código del
 #vendedor.
+
+#Ejercicio 222
+#Realiza una consulta que muestre un listado de los clientes y el monto total que nos han comprado 
+#de los 5 clientes que más han comprado. El listado debe tener las siguientes columnas: código del 
+#cliente, nombre del contacto, monto total. Ordenado por el monto total de forma descendente.
+
+#Ejercicio 223
+#Realiza una consulta que muestre un listado de los empleados de la empresa en los diferentes 
+#territorios. El listado debe tener las siguientes columnas: nombre del empleado ,id de la región y 
+#descripción del territorio ordenado por el nombre completo del empleado de forma ascendente
---
+++ Bases de datos/2ªEvaluacion/MySql/Practica/Northwind/Practica5c.sql
@@ -0,0 +1,90 @@
+#Ejercicio 301
+#Obtén los nombres de los clientes que han hecho pedidos después del 1/1/98.
+SELECT *
+FROM Customers;
+
+#Ejercicio 302
+#Obtén los nombres de los productos y de las compañías que los suministran. Los productos sin 
+#suministradores asignados y los suministradores sin productos asignados no se incluyen en el 
+#conjunto de resultados.
+
+#Ejercicio 303
+#Realiza una consulta que permita presentar el número de la factura y el código del cliente al cual 
+#ésta pertenece.
+
+#Ejercicio 304
+#Realiza una consulta que muestre el listado de productos en el cual se incluya el precio del 
+#producto, la diferencia entre el precio del producto y el precio promedio de todos los productos.
+#El listado debe incluir las siguientes columnas: nombre del producto, precio unitario, precio 
+#promedio de todos los productos, deferencia entre el precio promedio y el precio del producto.
+
+#Ejercicio 305
+#Realiza una consulta que muestre un listado de productos en el cual se incluya el porcentaje que el 
+#producto representa para todo el inventario.
+#El listado debe incluir las siguientes columnas: nombre del producto, monto del producto, monto 
+#total del inventario, porcentaje que representa el producto para el inventario.
+
+#Ejercicio 306
+#Realiza una consulta que muestre un listado donde se incluya los clientes que han comprado 
+#productos de todas las categorías. Las columnas deberán ser: código del cliente y nombre del 
+#cliente.
+
+#Ejercicio 307
+#Realiza una consulta que muestre el listado de aquellos productos cuyo promedio de unidades en 
+#pedidos sea mayor de 2.
+
+#Ejercicio 308
+#Muestra el importe total que ha gastado cada cliente.
+
+#Ejercicio 309
+#Realiza una consulta que muestre un listado de las Facturas de Cada Cliente.
+#El listado debe tener las siguientes columnas: Nombre del Cliente, País del Cliente, No. de Factura, 
+#Fecha de Factura ordenado por Nombre del Cliente de forma ascendente y luego por la fecha de la 
+#factura de forma descendente.
+
+#Ejercicio 310
+#Realiza una consulta que muestre un listado de los empleados que le han vendido a cada cliente. El 
+#listado debe tener las siguientes columnas: Nombre del Cliente, Nombre completo del Empleado 
+#ordenado por Nombre del Cliente de forma ascendente y luego por el Nombre completo del 
+#empleado de forma ascendente.
+
+#Ejercicio 311
+#Realiza una consulta que muestre un listado de los clientes y el número de facturas de cada cliente. 
+#El listado debe tener las siguientes columnas: Código Cliente, Nombre Cliente, total de facturas del 
+#Cliente.
+
+#Ejercicio 312
+#Realiza una consulta que muestre un listado de los clientes y el monto total que nos han comprado 
+#de los 5 clientes que más han comprado. El listado debe tener las siguientes columnas: Código del 
+#Cliente, Nombre Cliente, Monto Total gastado y ordenado por el Monto Total de forma 
+#descendente.
+
+#Ejercicio 313
+#Realiza una consulta que muestre un listado de las ventas que se han hecho por categoría. El listado 
+#debe tener las siguientes columnas: Nombre Categoría, Monto Total y ordenado por el Monto Total 
+#de Forma ascendente.
+
+#Ejercicio 314
+#Selecciona los datos de los productos con el nombre de la categoría a la que pertenece.
+
+#Ejercicio 315
+#Haz un listado que muestre el importe total de cada pedido.
+
+#Ejercicio 316
+#Haz un listado que muestre el stock de cada producto y el número de unidades vendidas.
+
+#Ejercicio 317
+#Modifica el listado anterior para poder filtrar las ventas por mes y año.
+
+#Ejercicio 318
+#Muestra un listado con las ventas totales que ha tenido cada cliente en cada año.
+
+#Ejercicio 319
+#Muestra un listado con las ventas totales de cada vendedor por año.
+
+#Ejercicio 320
+#Muestra un listado con las ventas totales por vendedor y cliente en un año determinado.
+
+#Ejercicio 321
+#Muestra un resumen de las ventas totales por cliente y categoría de productos en un año 
+#determinado
