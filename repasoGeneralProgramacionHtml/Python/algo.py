c44f26e70e3fa7b7ddd19f7e13fbe8d7ddbadd67
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2024-02-05 08:40:07 +0100

    practicas

--- repasoGeneralProgramacionHtml/Python/algo.py
+++ repasoGeneralProgramacionHtml/Python/algo.py
@@ -1,8 +1,5 @@
-numero1 = 0
-numero2 = 0
-
-input("Dame el numero 1: ")
-input("Dame el numero 2: ")
+numero1 = int(input("Dame el numero 1: "))
+numero2 = int(input("Dame el numero 2: "))
 
 if numero1 > numero2:
     print("El numero 1 es mayor que el numero 2")
