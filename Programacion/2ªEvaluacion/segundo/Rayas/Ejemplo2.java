432d7941797811937b14da8365dad469c8a6a7bf
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2024-02-07 21:15:01 +0100

    repasoArrays

---
+++ Programacion/2ªEvaluacion/segundo/Rayas/Ejemplo2.java
@@ -0,0 +1,13 @@
+package segundo.Rayas;
+
+import java.util.Scanner;
+
+public class Ejemplo2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+    
+}
